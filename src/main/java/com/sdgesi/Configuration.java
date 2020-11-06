54a1f36ac88bb007a7847c2161010dcf792de8f1
Lamont Girton <lgirton23@gmail.com>
2020-11-06 10:12:41 -0800

    Refactored to externalize configuration through application.properties with sensible defaults

---
+++ src/main/java/com/sdgesi/Configuration.java
@@ -0,0 +1,60 @@
+package com.sdgesi;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.activemq.RedeliveryPolicy;
+import org.apache.activemq.pool.PooledConnectionFactory;
+import org.apache.camel.CamelContext;
+import org.apache.camel.builder.component.ComponentsBuilderFactory;
+import org.apache.camel.component.activemq.ActiveMQComponent;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+@ApplicationScoped
+public class Configuration {
+
+    @ConfigProperty(name = "max.redeliveries", defaultValue = "20")
+    int maximumRedeliveries;
+
+    @ConfigProperty(name = "initial.redelivery.delay", defaultValue = "5000")
+    int initialRedeliveryDelay;
+
+    @ConfigProperty(name = "redelivery.delay", defaultValue = "5000")
+    int redeliveryDelay;
+
+    @ConfigProperty(name = "broker.url", defaultValue = "tcp://broker-amq:61616")
+    String brokerURL;
+
+
+    @Inject
+    CamelContext context;
+
+    @Named
+    ActiveMQComponent activemq() {
+        ActiveMQComponent component =
+                ComponentsBuilderFactory
+                        .activemq()
+                        .brokerURL(brokerURL)
+                        .errorHandlerLogStackTrace(false)
+                        .build();
+
+        component.setCamelContext(context);
+
+        configureRedeliveryPolicy(component);
+
+        return component;
+    }
+
+    private void configureRedeliveryPolicy(ActiveMQComponent component) {
+        PooledConnectionFactory connectionFactory = (PooledConnectionFactory) component.getOrCreateConnectionFactory();
+
+        ActiveMQConnectionFactory acf = (ActiveMQConnectionFactory) connectionFactory.getConnectionFactory();
+
+        RedeliveryPolicy redeliveryPolicy = acf.getRedeliveryPolicy();
+        redeliveryPolicy.setMaximumRedeliveries(maximumRedeliveries);
+        redeliveryPolicy.setInitialRedeliveryDelay(initialRedeliveryDelay);
+        redeliveryPolicy.setRedeliveryDelay(redeliveryDelay);
+    }
+}
--- src/main/java/com/sdgesi/IncomingRouteBuilder.java
+++ src/main/java/com/sdgesi/IncomingRouteBuilder.java
@@ -1,63 +1,54 @@
 package com.sdgesi;
 
-import org.apache.activemq.ActiveMQConnectionFactory;
-import org.apache.activemq.RedeliveryPolicy;
-import org.apache.activemq.pool.PooledConnectionFactory;
 import org.apache.camel.builder.RouteBuilder;
-import org.apache.camel.component.activemq.ActiveMQComponent;
-import org.apache.camel.component.jms.JmsQueueEndpoint;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 import javax.enterprise.context.ApplicationScoped;
-import javax.jms.Session;
 
 @ApplicationScoped
 public class IncomingRouteBuilder extends RouteBuilder {
-    private int maximumRedeliveries = 2;
-    private int initialRedeliveryDelay = 5000;
-    private int redeliveryDelay = 5000;
-    private String destinationName = "incoming.messages";
-    private String brokerURL = "tcp://localhost:61616";
+
+    @ConfigProperty(name = "log.exhausted", defaultValue = "false")
+    boolean logExhausted;
+
+    @ConfigProperty(name = "queue.name", defaultValue = "incoming.messages")
+    String destinationName;
+
+    @ConfigProperty(name = "rest.uri", defaultValue = "outgoing")
+    String restUri;
+
+    @ConfigProperty(name = "rest.host", defaultValue = "localhost:18080")
+    String restHost;
 
     @Override
-    public void configure() throws Exception {
+    public void configure() {
+
+        onException(Exception.class)
+                .logExhausted(logExhausted);
+
         rest("/incoming")
                 .id("incoming")
                 .post()
                 .to("direct:enqueue");
 
-
         from("direct:enqueue")
                 .id("enqueue")
-                .to("activemq:queue:incoming.messages")
+                .onException(Exception.class)
+                .maximumRedeliveries(0)
+                .handled(true)
+                .transform()
+                .simple("Error")
+                .end()
+                .toF("activemq:queue:%s", destinationName)
                 .transform()
                 .simple("Hello");
 
-        JmsQueueEndpoint endpoint = createJMSQueueEndpoint();
-
-
-        from(endpoint)
+        fromF("activemq:queue:%s?acknowledgementModeName=CLIENT_ACKNOWLEDGE", destinationName)
                 .id("dequeue")
-                .to("rest:post:outgoing?host=localhost:9000&bridgeEndpoint=true");
+                .toF("rest:post:%s?host=%s&bridgeEndpoint=true", restUri, restHost);
 
 
     }
 
-    private JmsQueueEndpoint createJMSQueueEndpoint() throws Exception {
-        ActiveMQComponent component = ActiveMQComponent
-                .activeMQComponent(brokerURL);
 
-        component.setCamelContext(getContext());
-
-        PooledConnectionFactory connectionFactory = (PooledConnectionFactory) component.getOrCreateConnectionFactory();
-        ActiveMQConnectionFactory acf = (ActiveMQConnectionFactory) connectionFactory.getConnectionFactory();
-        RedeliveryPolicy redeliveryPolicy = acf.getRedeliveryPolicy();
-        redeliveryPolicy.setMaximumRedeliveries(maximumRedeliveries);
-        redeliveryPolicy.setInitialRedeliveryDelay(initialRedeliveryDelay);
-        redeliveryPolicy.setRedeliveryDelay(redeliveryDelay);
-
-        JmsQueueEndpoint endpoint = (JmsQueueEndpoint) component.createEndpoint("activemq:queue");
-        endpoint.setDestinationName(destinationName);
-        endpoint.setAcknowledgementMode(Session.CLIENT_ACKNOWLEDGE);
-        return endpoint;
-    }
 }
